52cb62abaaebb7c8016cd907e4fffec98532bbc7
Atanas Atanasov <scriptex.bg@gmail.com>
2019-09-09 18:51:26 +0300

    WIP: CSS extract

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,10 @@
-import fs from 'fs';
-
-import sass from 'rollup-plugin-sass';
 import babel from 'rollup-plugin-babel';
-import uglify from 'rollup-plugin-uglify';
 import replace from 'rollup-plugin-replace';
 import resolve from 'rollup-plugin-node-resolve';
-import postcss from 'postcss';
+import postcss from 'rollup-plugin-postcss';
 import commonjs from 'rollup-plugin-commonjs';
 import stylelint from 'rollup-plugin-stylelint';
+import { uglify } from 'rollup-plugin-uglify';
 import { eslint } from 'rollup-plugin-eslint';
 import browsersync from 'rollup-plugin-browsersync';
 
@@ -18,68 +15,63 @@ import flexbugsFixes from 'postcss-flexbugs-fixes';
 
 const isProd = process.env.NODE_ENV === 'production';
 
-export default {
-	input: 'src/js/index.js',
-	output: {
-		dir: 'dist',
-		format: 'iife',
-		sourceMap: isProd ? false : 'inline'
-	},
-	plugins: [
-		resolve({
-			mainFields: ['jsnext:main', 'module', 'main', 'browser']
-		}),
-		commonjs(),
-		sass({
-			output(styles, styleNodes) {
-				styleNodes.forEach(({ id, content }) => {
-					const scssName = id.substring(id.lastIndexOf('/') + 1, id.length);
-					const name = scssName.split('.')[0];
-					fs.writeFileSync(`dist/${name}.css`, content);
-				});
-			},
-			processor: css => {
-				console.log(
-					css,
-					postcss({
-						from: 'src/scss/index.scss',
-						plugins: [easyImport, postcssUrl, flexbugsFixes, autoprefixer]
-					})
-						.process(css)
-						.then(result => result.css)
-				);
-
-				return postcss({
-					from: 'src/scss/index.scss',
-					plugins: [easyImport, postcssUrl, flexbugsFixes, autoprefixer]
-				})
-					.process(css)
-					.then(result => result.css);
-			}
-		}),
-		stylelint({
-			include: ['src/**/*.scss'],
-			syntax: 'scss'
-		}),
-		eslint({
-			exclude: ['src/scss/**', 'dist/**']
-		}),
-		babel({
-			exclude: 'node_modules/**'
-		}),
-		replace({
-			exclude: 'node_modules/**',
-			ENV: JSON.stringify(process.env.NODE_ENV || 'development')
-		}),
-		isProd && uglify(),
-		!isProd &&
-			browsersync({
-				host: 'localhost',
-				watch: true,
-				port: 3000,
-				notify: false,
-				open: true
+export default [
+	{
+		input: 'src/scss/index.scss',
+		output: {
+			file: 'dist/app.css',
+			format: 'es'
+		},
+		plugins: [
+			stylelint({
+				include: ['src/**/*.css'],
+				syntax: 'scss'
+			}),
+			postcss({
+				modules: true,
+				plugins: [easyImport(), postcssUrl({ url: 'rebase' }), flexbugsFixes(), autoprefixer()],
+				extensions: ['.css', '.scss', '.sass'],
+				minimize: isProd,
+				parser: 'postcss-scss',
+				syntax: 'postcss-scss'
 			})
-	],
-	external: 'stylelint'
-};
+		]
+	},
+	{
+		input: 'src/js/index.js',
+		output: {
+			file: 'dist/app.js',
+			format: 'iife',
+			sourceMap: isProd ? false : 'inline'
+		},
+		plugins: [
+			resolve({
+				mainFields: ['jsnext:main', 'module', 'main', 'browser']
+			}),
+			commonjs(),
+			replace({
+				exclude: 'node_modules/**',
+				ENV: JSON.stringify(process.env.NODE_ENV || 'development')
+			}),
+			eslint({
+				exclude: ['src/scss/**', 'dist/**']
+			}),
+			babel({
+				exclude: 'node_modules/**'
+			}),
+
+			isProd && uglify(),
+			!isProd &&
+				browsersync({
+					host: 'localhost',
+					port: 3000,
+					open: true,
+					watch: true,
+					files: ['*.html', '*.php', 'src/**/*'],
+					proxy: 'localhost',
+					notify: false
+				})
+		],
+		external: 'stylelint'
+	}
+];
--- src/js/index.js
+++
@@ -1,9 +0,0 @@
-const test = '1';
-
-alert(test);
-
-const log = message => {
-	console.log(message);
-};
-
-log('This is message');
